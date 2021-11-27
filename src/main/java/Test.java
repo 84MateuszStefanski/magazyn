3e0c2930dc27b2357798875a09d9f45a3166d7c3
84MateuszStefanski <84mateuszstefanski@gmail.com>
2021-11-27 16:13:00 +0100

    encryption of adminn password and login

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,24 +2,30 @@ import daoservices.ProductSearchEngine;
 import entities.Product;
 import org.hibernate.Session;
 import utils.HibernateUtil;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import javax.persistence.NoResultException;
 
 public class Test {
 
-    public static void main(String[] args) throws NoResultException{
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+//    public static void main(String[] args) throws NoResultException{
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        session.beginTransaction();
+//
+//      try {
+//          System.out.println(ProductSearchEngine.getProductList(session));
+//      }catch (NoResultException e){
+//          e.printStackTrace();
+//      }
+//
+//        session.getTransaction().commit();
+//        HibernateUtil.close();
+//
+//    }
 
-      try {
-          System.out.println(ProductSearchEngine.getProductList(session));
-      }catch (NoResultException e){
-          e.printStackTrace();
-      }
-
-        session.getTransaction().commit();
-        HibernateUtil.close();
-
+    public static void main(String[] args) {
+        String hash = DigestUtils.sha256Hex("password");
+        System.out.println(hash);
     }
 
 }
